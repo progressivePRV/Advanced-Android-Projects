8350e5cecdea86ad2e4522ee8fe45d181198f89d
progressivePRV <94014.prabhav@gmail.com>
2020-09-12 23:06:19 -0400

    there were same methods in UserProfile, removed it

--- ChatApp/app/src/main/java/com/helloworld/myapplication/UserProfile.java
+++ ChatApp/app/src/main/java/com/helloworld/myapplication/UserProfile.java
@@ -81,59 +81,5 @@ public class UserProfile implements Serializable {
         this.profileImage = profileImage;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getProfileImage() {
-        return profileImage;
-    }
-
-    public void setProfileImage(String profileImage) {
-        this.profileImage = profileImage;
-    }
 }
